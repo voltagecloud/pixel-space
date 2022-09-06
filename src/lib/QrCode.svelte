d2bf095fee4d609f8fb40a70ea7194c6d8af3ce0
Adrien <gaarf@users.noreply.github.com>
2022-09-05 18:22:37 -0700

    add qrcode

---
+++ src/lib/QrCode.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import QRCode from 'qrcode';
+
+  const qr = new QRCode();
+
+  export let value = "";
+  let data: Promise<string>;
+
+  $: if(value) { data = QRCode.toDataURL(value); }
+</script>
+
+{#await data then src}
+  <img {src} alt={value} />  
+{/await}
