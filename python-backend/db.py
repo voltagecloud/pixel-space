239c4215ddc64c4ef70fd014576939f83e488b7a
Sam Korn <korn94sam@gmail.com>
2022-09-07 16:41:05 -0600

    learn more about orm

--- python-backend/db.py
+++ python-backend/db.py
@@ -3,17 +3,32 @@ import code
 from datetime import datetime
 
 def main() -> None:
-    db = Prisma()
-    db.connect()
+    # db = Prisma(
+    #     http={
+    #         'timeout': 10,
+    #     },
+    # )
+
+    with Prisma() as db:
+    # db.connect()
 
     # write your queries here
-    code.interact(local=dict(globals(), **locals()))
-# db.pixel.create({
-#     "id": 1234,
-#     "color": "test123",
-#     "purchases": [],
-#     "updatedAt": datetime.now()
-# })
+        code.interact(local=dict(globals(), **locals()))
+    # db.pixel.create({
+    #     "id": 1234,
+    #     "color": "test123",
+    #     "purchases": [],
+    #     "updatedAt": datetime.now()
+    # })
+    db.pixel.upsert(where={"id": 0}, data={"create": {}, "update": {}})
+    db.pixel.create(data={})
+    db.pixel.query_raw(query="SELECT * FROM Pixel")
+    db.pixel.find_many(take=5)
+    apixel = db.pixel.find_many(take=5)[0]
+
+    db.purchase.create(data={ "color": "ffffff", "pixels":[ apixel ] })
+    db.purchase.create(data={"color":"red"})
+    db.purchase.create(data={"color":"red", "complete": False})
     db.disconnect()
 
 if __name__ == '__main__':
