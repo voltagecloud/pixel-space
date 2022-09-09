08978230aefae33376391ba94a46efa18665bf11
Adrien <gaarf@users.noreply.github.com>
2022-09-09 14:06:15 -0700

    throw await res.json

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,14 @@
 export async function postJson<T>(url: string, data: unknown) {
-  const res = await fetch(url, {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: { 'Content-Type': 'application/json' }
-  });
-  return res.json() as Promise<T>;
+	const res = await fetch(url, {
+		method: 'POST',
+		body: JSON.stringify(data),
+		headers: { 'Content-Type': 'application/json' }
+	});
+	if (!res.ok) {
+		throw await res.json().then(
+			({ message }) => message,
+			() => 'postJson failed'
+		);
+	}
+	return res.json() as Promise<T>;
 }
