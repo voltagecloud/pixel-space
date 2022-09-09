754240277dc32e68afce9ec5f1d188a84c08158f
Sam Korn <korn94sam@gmail.com>
2022-09-09 13:46:12 -0600

    seed with pepe

--- python-backend/seed.py
+++ python-backend/seed.py
@@ -2,13 +2,22 @@ from prisma import Prisma
 import code
 from datetime import datetime
 
+import webcolors
+from PIL import Image
+
+webcolors.rgb_to_hex((61, 34, 57))
+
 def main() -> None:
     # Board size: 50 x 50
+    img = Image.open("../pepe.png")
+    img = img.convert("RGB")
+    colors_rgb = list(img.getdata())
+    colors_hex = [webcolors.rgb_to_hex(rgb) for rgb in colors_rgb ]
 
     with Prisma() as db:
-        for i in range(0,2500):
+        for i in colors_hex:
             print(f"adding pixel: {i}")
-            db.pixel.create(data={"color": "#ffffff"})
+            db.pixel.create(data={"color": i})
 
 if __name__ == '__main__':
     main()
